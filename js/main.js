36697e4b006a650f6b198ead1bc5a8cda13e2c2a
Kyle Skogen <sfg@sfgs-MacBook-Pro.local>
2014-01-06 15:11:06 -0600

    adding social buttons, bio-pic, some other temp content

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
+$(document).ready(function() {
+	
+	$(".social-btn").fitText(0.25);
 
+	var middlefy = function() {
+		$(".social-button-wrap i").each(function() {
+			var iconHeight = $(this).height();
+			var buttonHeight = $(this).parents(".social-btn").height();
+			var math = (buttonHeight - iconHeight) / 2;
+			$(this).css("top", math);
+		});
+	};
+	middlefy();
+
+	$(window).resize(function() {
+		middlefy();
+	});
+});
